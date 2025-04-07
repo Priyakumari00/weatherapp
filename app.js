3f97558ad56c0fa3eccdccc2b85c04112e67c122
Priya Kumari <priya.k.p.13579@gmail.com>
2025-04-07 09:06:50 +0530

    done

--- app.js
+++ app.js
@@ -9,9 +9,21 @@ app.get("/", function(req, res){
     const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=fd1c4c1245602771f578a769d4a82a22"
     https.get(url,function(response){
         console.log(response);
-    })
 
-    res.send("server is up and running");
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageUrl = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+            res.write("<p> The weather is currently " + weatherDescription  + " <p/>");
+            res.write(" <h1> The Temperature in London is " + temp +  "  degree celcius. <h1/>");
+            res.write("<img src = " + imageUrl + ">");
+            res.send();
+
+        })
+    })
+    
 })
 
 
